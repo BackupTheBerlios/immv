9b91b34a01326b6820f244c9de6441ec543a803b
Marco Herrn <mherrn@denkbrett.herrn.local>
2009-08-17 20:39:24 +0200

    Code cleaning
    * removed obsolete functions
    * more comments
    * no camelCase

--- immv.py
+++ immv.py
@@ -8,21 +8,34 @@ import subprocess
 import shutil
 import tempfile
 
+
+################################################################
+##
+## Exceptions
+##
+
 class FileNotFoundException(Exception):
   def __init__(self, filename):
+    Exception.__init__(self)
     self.filename= filename
   def __str__(self):
     return repr(self.filename)
 
 class FileCountChangedException(Exception):
   def __init__(self, orig_count, new_count):
+    Exception.__init__(self)
     self.orig_count= orig_count
     self.new_count= new_count
   def __str__(self):
-    return "original count="+str(self.orig_count)+", new count="+str(self.new_count)
+    return "original count="+str(self.orig_count)+\
+           ", new count="+str(self.new_count)
 
+################################################################
+##
+## Functions
+##
 
-def parseCommandline():
+def parse_commandline():
   usage= "usage: %prog [options] FILES"
   parser= OptionParser(usage=usage)
   parser.add_option("-a", "--ask", dest="needs_confirmation", default=False,
@@ -31,33 +44,12 @@ def parseCommandline():
   parser.add_option("-q", "--quiet", dest="verbose", default=True,
                     action="store_false",
                     help="don't output anything, except error messages")
+  parser.add_option("-o", "--overwrite", dest="overwrite_files", default=False,
+                    action="store_true",
+                    help="overwrite existing files")
 
   (options, args)= parser.parse_args()
   return (options, args)
-
-
-def get_file_list(filenames):
-  file_list= []
-  for f in filenames:
-    file_list.append(open(f))
-  return file_list
-
-
-def filenames_to_string(filenames):
-  fn_string= ""
-  for f in filenames:
-    if not os.path.lexists(f):
-      raise FileNotFoundException(f)
-    fn_string+= os.path.basename(f)+"\n"
-  return fn_string
-
-def get_base_file_names(filenames):
-  base_file_names= []
-  for f in filenames:
-    if not os.path.lexists(f):
-      raise FileNotFoundException(f)
-    base_file_names.append(os.path.basename(f))
-  return base_file_names
 
 
 def split_path_and_filenames(filenames):
@@ -70,7 +62,6 @@ def split_path_and_filenames(filenames):
     paths.append(path)
     base_file_names.append(basename)
   return paths, base_file_names
-  
     
 
 def check_files_exist(filenames):
@@ -79,7 +70,7 @@ def check_files_exist(filenames):
       raise FileNotFoundException(f)
 
 
-def callEditorForEditing(temp_file):
+def call_editor(temp_file):
   env_editor= os.getenv("EDITOR")
   if env_editor is None:
     #FIXME: is this too debian specific
@@ -87,19 +78,9 @@ def callEditorForEditing(temp_file):
   retcode= subprocess.call([env_editor, temp_file.name])
 
 
-def get_files_to_rename(old_files, new_files):
-  if len(old_files) != len(new_files):
-    raise FileCountChangedException(len(old_files), len(new_files))
-
-  files_to_rename= []
-  for i in range(0, len(old_files)):
-    if old_files[i] != new_files[i]:
-      files_to_rename.append((old_files[i], new_files[i]))
-  
-  return files_to_rename
-
 def get_temp_file():
   return tempfile.NamedTemporaryFile(mode='w+')
+
 
 def fill_temp_file(old_files, temp_file):
   print 1
@@ -109,6 +90,7 @@ def fill_temp_file(old_files, temp_file):
   print 3
   temp_file.flush()
 
+
 def strip_eol(list_of_strings):
   stripped= []
   for item in list_of_strings:
@@ -116,47 +98,49 @@ def strip_eol(list_of_strings):
   return stripped
 
 
+def do_rename(paths, base_file_names, new_file_names):
+  for path, orig_filename, new_filename in \
+      zip(paths, base_file_names, new_file_names):
+    if not orig_filename == new_filename:
+      #TODO: prüfe, ob destfile schon existiert + Exception
+      shutil.move(os.path.join(path, orig_filename),\
+                    os.path.join(path, new_filename))
+
+
+################################################################
+##
+## main
+##
+
 if __name__ == "__main__":
   #parse Kommandozeile
-  (options, args)= parseCommandline()
+  (options, args)= parse_commandline()
   #prüfe, ob auch wirklich alles Dateinamen sind
   try:
-#    #Erstelle String aus Dateinamen
-#    fn_string= filenames_to_string(args)
-#    base_file_names= get_base_file_names(args)
-#    paths_of_file_names= get_paths_of_file_names(args)
+    #Trenne Pfade von Dateinamen
     paths, base_file_names= split_path_and_filenames(args)
 
-    #erzeuge temp_file
+    #Erzeuge temp_file
     temp_file= get_temp_file()
     fill_temp_file(base_file_names, temp_file)
 
-    #zeige Editor mit temp_file
-    callEditorForEditing(temp_file)
+    #Zeige Editor mit temp_file
+    call_editor(temp_file)
 
-    #lies geändertes temp_file
+    #Lies geändertes temp_file
     temp_file.seek(0)
     lines= temp_file.readlines()
     temp_file.close()
     new_file_names= strip_eol(lines)
 
-    for path, orig_filename, new_filename in \
-        zip(paths, base_file_names, new_file_names):
-      print "P: %s, OFN: %s, NFN: %s" % (path, orig_filename, new_filename)
-      if not orig_filename == new_filename:
-        #TODO: prüfe, ob destfile schon existiert + Exception
-        shutil.move(os.path.join(path, orig_filename),\
-                    os.path.join(path, new_filename))
+
+    #Führe das Umbenennen durch
+    do_rename(paths, base_file_names, new_file_names)
       
   except FileNotFoundException, e:
     print "Error! File not found: "+e.filename
-    exit(1)
+    exit(2)
   except FileCountChangedException, e:
     print "ERROR! Number of files changed: ",e
-    exit(2)
+    exit(3)
 
-#  file_list= get_file_list(args)
-#  print file_list
-  #Zeige String in $EDITOR
-  #prüfe Anzahl Zeilen in origString + newString
-  #mache Änderungen
